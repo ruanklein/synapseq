067461c1100563d9f7549e49bd888435fd4a94c7
Ruan <ruan.klein@gmail.com>
2025-08-20 23:23:46 -0300

    chore: moved amplitude to default audio package

--- internal/audio/audio.go
+++ internal/audio/audio.go
@@ -3,4 +3,18 @@ package audio
 const (
 	NumberOfChannels = 16    // Number of channels
 	SineTableSize    = 16384 // Number of elements in sine-table (power of 2)
+
+	amplitudeScale = 40.96 // Amplitude scale factor
 )
+
+type AmplitudeType float64 // Amplitude level (0-4096 for 0-100%)
+
+// ToPercent converts a raw amplitude value to a float64 percentage
+func (a AmplitudeType) ToPercent() float64 {
+	return float64(a / amplitudeScale)
+}
+
+// AmplitudePercentToRaw converts a float64 value to a raw amplitude value
+func AmplitudePercentToRaw(v AmplitudeType) float64 {
+	return float64(v) * amplitudeScale
+}
