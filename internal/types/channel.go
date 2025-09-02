6ebaa6f0724ed1cdce16ef7aae4425be7ba825fd
Ruan <ruan.klein@gmail.com>
2025-09-01 21:16:20 -0300

    chore: method for count active channels (non off)

--- internal/types/channel.go
+++ internal/types/channel.go
@@ -8,3 +8,13 @@ type Channel struct {
 	Increment [2]int    // Increment (for binaural tones, offset + increment into sine table * 65536)
 	Offset    [2]int    // Offset 1
 }
+
+// CountActiveChannels counts the number of active channels
+func CountActiveChannels(chs []Channel) int {
+	for i := len(chs) - 1; i >= 0; i-- {
+		if chs[i].Voice.Type != VoiceOff {
+			return i + 1
+		}
+	}
+	return 1 // At least 1 channel always
+}
