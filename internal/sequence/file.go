8e0261d6f78d65d33308538fa3625c0edd3e7429
Ruan <ruan.klein@gmail.com>
2025-11-05 07:50:05 -0300

    chore: uses getFile to create sequence file instance

--- internal/sequence/file.go
+++ internal/sequence/file.go
@@ -9,75 +9,27 @@ package sequence
 
 import (
 	"bufio"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"strings"
-)
 
-// maxFileSize defines the maximum file size (32KB)
-const maxFileSize = 32 * 1024
+	s "github.com/ruanklein/synapseq/v3/internal/shared"
+	t "github.com/ruanklein/synapseq/v3/internal/types"
+)
 
 // SequenceFile represents a sequence file
 type SequenceFile struct {
 	CurrentLine       string         // Current line in the file
 	CurrentLineNumber int            // Current line number
 	scanner           *bufio.Scanner // Scanner for reading the file
-	file              *os.File       // File handle
-}
-
-// loadRemoteFile loads a remote file from a given URL (max 32KB and text/plain)
-func loadRemoteFile(url string) (io.Reader, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("error fetching remote file: %v", err)
-	}
-	defer resp.Body.Close()
-
-	contentType := resp.Header.Get("Content-Type")
-	if !strings.HasPrefix(contentType, "text/plain") {
-		return nil, fmt.Errorf("invalid content-type: %s", contentType)
-	}
-
-	data, err := io.ReadAll(io.LimitReader(resp.Body, maxFileSize))
-	if err != nil {
-		return nil, fmt.Errorf("error reading remote file: %v", err)
-	}
-
-	return strings.NewReader(string(data)), nil
 }
 
 // LoadFile loads a sequence file
 func LoadFile(fileName string) (*SequenceFile, error) {
-	if fileName == "-" {
-		reader := io.LimitReader(os.Stdin, maxFileSize)
-		return &SequenceFile{
-			scanner: bufio.NewScanner(reader),
-			file:    nil,
-		}, nil
-	}
-
-	if strings.HasPrefix(fileName, "http://") || strings.HasPrefix(fileName, "https://") {
-		reader, err := loadRemoteFile(fileName)
-		if err != nil {
-			return nil, err
-		}
-		return &SequenceFile{
-			scanner: bufio.NewScanner(reader),
-			file:    nil,
-		}, nil
-	}
-
-	file, err := os.Open(fileName)
+	file, err := s.GetFile(fileName, t.FormatText)
 	if err != nil {
-		return nil, fmt.Errorf("%v", err)
+		return nil, err
 	}
-	reader := io.LimitReader(file, maxFileSize)
 
 	return &SequenceFile{
-		scanner: bufio.NewScanner(reader),
-		file:    file,
+		scanner: bufio.NewScanner(file),
 	}, nil
 }
 
@@ -93,11 +45,4 @@ func (sf *SequenceFile) NextLine() bool {
 		return true
 	}
 	return false
-}
-
-// Close closes the sequence file
-func (sf *SequenceFile) Close() {
-	if sf.file != nil {
-		sf.file.Close()
-	}
 }
