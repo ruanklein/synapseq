a92aaf09779e2f989536626eb02fa6c3ce14b53e
Ruan <ruan.klein@gmail.com>
2025-12-07 10:21:07 -0300

    test: updated for external

--- external/example_test.go
+++ external/example_test.go
@@ -64,7 +64,7 @@ func ExampleNewFFmpeg() {
 	// ffmpeg initialized: ffmpeg
 }
 
-func ExampleFFmpeg_Convert_mp3() {
+func ExampleFFmpeg_Convert() {
 	// Create SynapSeq application context
 	ctx, err := synapseq.NewAppContext("input.spsq", "output.mp3", "text")
 	if err != nil {
@@ -80,90 +80,12 @@ func ExampleFFmpeg_Convert_mp3() {
 	// 	log.Fatal(err)
 	// }
 
-	// Encode MP3 using VBR mode (highest quality)
-	// opts := &external.CodecOptions{
-	// 	MP3Options: &external.MP3Options{Mode: external.MP3ModeVBR},
-	// }
-	// _ = encoder.Convert(ctx, "mp3", opts)
+	// Encode MP3 at 320 kbps CBR
+	// _ = encoder.Convert(ctx, "mp3")
 
-	fmt.Printf("MP3 encoding (VBR) executed successfully for output: %s\n", ctx.OutputFile())
+	fmt.Printf("MP3 encoding executed successfully for output: %s\n", ctx.OutputFile())
 	// Output:
-	// MP3 encoding (VBR) executed successfully for output: output.mp3
-}
-
-func ExampleFFmpeg_Convert_mp3CBR() {
-	// Create SynapSeq application context
-	ctx, err := synapseq.NewAppContext("input.spsq", "output.mp3", "text")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Load sequence (required before encoding)
-	// _ = ctx.LoadSequence()
-
-	// Create ffmpeg instance
-	// encoder, err := external.NewFFmpeg("")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// Encode MP3 using CBR mode at 320 kbps
-	// opts := &external.CodecOptions{
-	// 	MP3Options: &external.MP3Options{Mode: external.MP3ModeCBR},
-	// }
-	// _ = encoder.Convert(ctx, "mp3", opts)
-
-	fmt.Printf("MP3 encoding (CBR) executed successfully for output: %s\n", ctx.OutputFile())
-	// Output:
-	// MP3 encoding (CBR) executed successfully for output: output.mp3
-}
-
-func ExampleFFmpeg_Convert_ogg() {
-	// Create SynapSeq application context
-	ctx, err := synapseq.NewAppContext("input.spsq", "output.ogg", "text")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Load sequence (required before encoding)
-	// _ = ctx.LoadSequence()
-
-	// Create ffmpeg instance
-	// encoder, err := external.NewFFmpeg("")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// Encode OGG/Vorbis at highest quality
-	// _ = encoder.Convert(ctx, "ogg", nil)
-
-	fmt.Printf("OGG encoding executed successfully for output: %s\n", ctx.OutputFile())
-	// Output:
-	// OGG encoding executed successfully for output: output.ogg
-}
-
-func ExampleFFmpeg_Convert_opus() {
-	// Create SynapSeq application context
-	ctx, err := synapseq.NewAppContext("input.spsq", "output.opus", "text")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Load sequence (required before encoding)
-	// _ = ctx.LoadSequence()
-
-	// Create ffmpeg instance
-	// encoder, err := external.NewFFmpeg("")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// Encode OPUS at 96 kbps (requires 48000 Hz sample rate)
-	// _ = encoder.Convert(ctx, "opus", nil)
-
-	fmt.Printf("OPUS encoding executed successfully for output: %s\n", ctx.OutputFile())
-	// Output:
-	// OPUS encoding executed successfully for output: output.opus
+	// MP3 encoding executed successfully for output: output.mp3
 }
 
 func ExampleNewFFprobe() {
